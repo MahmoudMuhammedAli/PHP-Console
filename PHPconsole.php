0fe4b23baf809d9c91311c3dd9dec890086cf998
Mahmoud Ali <ibn.el4ai5@gmail.com>
2020-11-27 17:46:35 +0200

    first commit

---
+++ PHPconsole.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> PHP Console</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles.css">
+        <link rel="icon" type="image/png" href="pics/php.png"/>
+    </head>
+    <body>
+       
+
+        <p><h1>PHP Console!</h1>
+        <br>
+        - How does it work?
+    </p>
+    <?php
+    echo"= Insert your code into the PHP file and see for your self!";
+    ?>
+    
+    <div class="console">
+        <form action="ftry.php" method="get">
+            <!-- <input type="text" name="a"> -->
+        </form>
+    <hr>    
+    <?php
+        //Insert php code here!
+
+    ?>
+    </div>
+    </body>
+</html>
